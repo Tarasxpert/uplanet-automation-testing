5a05ccc41a49a554c2034ebaacc43ff3e47bdfa5
Ilya <98581711+Qedi231@users.noreply.github.com>
2023-03-13 17:02:33 +0200

    Create selenideWebDriver

--- src/test/java/ui/tests/LoginTest.java
+++ src/test/java/ui/tests/LoginTest.java
@@ -3,13 +3,13 @@ package ui.tests;
 import io.qameta.allure.Description;
 import io.qameta.allure.Owner;
 import org.testng.annotations.Test;
-import ui.base.BrowserStackDriver;
+import ui.base.SelenideBaseDriver;
 import ui.pages.CloudcampaignCRMDashboardPage;
 import ui.pages.CloudcampaignCRMLoginPage;
 
 import static org.testng.Assert.assertTrue;
 
-public class LoginTest extends BrowserStackDriver {
+public class LoginTest extends SelenideBaseDriver {
     @Test(description= "Login test in https://app.qa.cloudcampaign.com")
     @Owner("Taras Zelenskyi")
     @Description("Logging in to CRM using existing credentials and checking if dashboard is displayed and visible.")
